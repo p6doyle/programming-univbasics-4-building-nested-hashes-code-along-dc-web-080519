b722a6d73c170d75523d48b7542dce99172b5dd7
Peyton Doyle <p6doyle@gmail.com>
2019-07-19 17:21:29 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,8 +23,7 @@ def bonus
   epic_tragedy[:montague][:hero][:status] = "dead"
   puts epic_tragedy
   
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
-  puts epic_tragedy
+ 
 
   epic_tragedy
 end
